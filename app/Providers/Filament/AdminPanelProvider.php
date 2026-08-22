aa98663fa2a921857b84e50ee386670e98caf40d
Mostafa Salem <mostafasalem25682@gmail.com>
2026-08-22 22:58:32 +0300

    Change font to Cairo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->font('Cairo')
             ->colors([
                 'primary' => Color::Amber,
             ])
